b0bc77b64eda8f1f58e39fd612b52d4cfc31056c
Zan <zanasmakarov@gmail.com>
2020-07-10 11:18:11 +0100

    html-backbone done, css - basic styling, js hardcoded colors and guessing logic

---
+++ game.js
@@ -0,0 +1,33 @@
+var colors = [
+    "rgb(255, 0, 0)",
+    "rgb(255, 255, 0)",
+    "rgb(0, 255, 0)",
+    "rgb(0, 255, 255)",
+    "rgb(0, 0, 255)",
+    "rgb(255, 0, 255)",
+]
+
+var squares = document.querySelectorAll(".square");
+var pickedColor = colors[3];
+var colorDisplay = document.querySelector("#ColorDisplay");
+
+colorDisplay.textContent=pickedColor;
+
+for(var i= 0; i<squares.length;i++){
+    //add initial colors
+    squares[i].style.backgroundColor = colors[i];
+
+    //add click listeners to squares
+    squares[i].addEventListener("click",function(){
+        //grab color of clicked square
+        var clickedColor = this.style.backgroundColor;
+        // compare to picked color
+        if (clickedColor === pickedColor){
+            alert("Correct");
+        } else {
+            alert("wrong");
+        }
+    })
+
+}
+
