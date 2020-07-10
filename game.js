6adba127a5183ddb442417809373548ea1112912
Zan <zanasmakarov@gmail.com>
2020-07-10 13:29:18 +0100

    logic for selecting random colors, changing h1 to correct color

--- game.js
+++ game.js
@@ -1,16 +1,10 @@
-var colors = [
-    "rgb(255, 0, 0)",
-    "rgb(255, 255, 0)",
-    "rgb(0, 255, 0)",
-    "rgb(0, 255, 255)",
-    "rgb(0, 0, 255)",
-    "rgb(255, 0, 255)",
-]
+var colors = generateRandomColors(6);
 
 var squares = document.querySelectorAll(".square");
 var pickedColor = pickColor();
 var colorDisplay = document.querySelector("#ColorDisplay");
-var messageDisplay = document.querySelector("#message")
+var messageDisplay = document.querySelector("#message");
+var h1 = document.querySelector("h1");
 
 colorDisplay.textContent=pickedColor;
 
@@ -26,6 +20,7 @@ for(var i= 0; i<squares.length;i++){
         if (clickedColor === pickedColor){
             messageDisplay.textContent = "Correct"
             changeColors(clickedColor);
+            h1.style.backgroundColor = clickedColor;
         } else {
             this.style.backgroundColor = "#232323"
             messageDisplay.textContent="Try Again"
@@ -44,4 +39,24 @@ function changeColors(color){
 function pickColor(){
     var random = Math.floor(Math.random() * colors.length +1);
     return colors[random]
+}
+
+function generateRandomColors(num){
+    //make an array
+    var arr = []
+    //add num random colors to array
+    for(var i = 0; i < num; i++){
+        //get random color and push into arr
+        arr.push(randomColor());
+    }
+    //return that array
+    return arr
+}
+
+function randomColor(){
+    var r = Math.floor(Math.random() * 256);
+    var g = Math.floor(Math.random() * 256);
+    var b = Math.floor(Math.random() * 256);
+    return `rgb(${r}, ${g}, ${b})`
+
 }
